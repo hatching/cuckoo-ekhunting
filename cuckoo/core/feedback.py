f1c324e2d3f62c1eccb7030ff178c71402ebbe1b
Ricardo van Zutphen <ricardo@cuckoo.sh>
2017-11-22 19:28:16 +0100

    change feedback endpoint from test url to real url

--- cuckoo/core/feedback.py
+++ cuckoo/core/feedback.py
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)
 
 class CuckooFeedback(object):
     """Contacts Cuckoo HQ with feedback & optional analysis dump."""
-    endpoint = "http://127.0.0.1:9086/lol"
+    endpoint = "https://feedback.cuckoosandbox.org/api/submit/"
     exc_whitelist = (
         CuckooFeedbackError,
     )
