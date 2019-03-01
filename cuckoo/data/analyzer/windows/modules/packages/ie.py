295639cdb1ccee25c350aadb1926141e93fcdde7
Ricardo van Zutphen <ricardo@hatching.io>
2019-03-01 19:48:58 +0100

    Use correct flags for IE starting

--- cuckoo/data/analyzer/windows/modules/packages/ie.py
+++ cuckoo/data/analyzer/windows/modules/packages/ie.py
@@ -133,7 +133,8 @@ class IE(Package):
         pids = []
         for url in target:
             pid = self.execute(
-                iexplore, args=["-nomerge", url], maximize=True,
+                iexplore,
+                args=["-noframemerging", "-private", url], maximize=True,
                 mode="iexplore"
             )
 
